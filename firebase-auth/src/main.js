1f350fb7ebbb6139bff778e4570fac46495187a7
Kiirisuna <31201780+Kiirisuna@users.noreply.github.com>
2020-05-28 14:53:29 +0800

    Update main.js

--- firebase-auth/src/main.js
+++ firebase-auth/src/main.js
@@ -8,9 +8,9 @@ Vue.config.productionTip = false;
 
 const configOptions = {
   apiKey: "",
-  authDomain: "vue-firebase-auth-2802d.firebaseapp.com",
-  databaseURL: "https://vue-firebase-auth-2802d.firebaseio.com",
-  projectId: "vue-firebase-auth-2802d",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
   messagingSenderId: "",
   appId: ""
